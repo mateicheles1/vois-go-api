41f32c90b526e8061c75f7fd2fc17c57c9a9b426
Matei Cheles <mateicheles1@gmail.com>
2023-03-20 16:04:24 +0200

    adding logger

--- logs/logger.go
+++ logs/logger.go
@@ -9,16 +9,19 @@ import (
 )
 
 func Logger() zerolog.Logger {
+
 	logFile, err := os.OpenFile(
         "./logs/log-file.log",
         os.O_RDWR|os.O_CREATE|os.O_APPEND,
         0664,
     )
+
 	if err != nil {
 		log.Fatal().
 		Err(err).
 		Msg("Couldn't open file")
 	}
+	
 	zerolog.TimeFieldFormat = zerolog.TimeFormatUnix
 	zerolog.ErrorStackMarshaler = pkgerrors.MarshalStack
 	logger := zerolog.New(logFile).With().Timestamp().Caller().Logger()
