0ccacc882f68e9ec66cc0ca17e98989a297d18a4
Matei Cheles <mateicheles1@gmail.com>
2023-04-03 11:03:01 +0300

    db

--- initializers/db.go
+++ initializers/db.go
@@ -2,6 +2,7 @@ package initializers
 
 import (
 	"gogin-api/logs"
+	"os"
 
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
@@ -11,9 +12,8 @@ var DB *gorm.DB
 
 func ConnectToDB() {
 	var err error
-	dsn := "host=ruby.db.elephantsql.com user=dshfydzd password=YVKi7Ce2-a3kdDuTAk7s1ZIah74lO_q- dbname=dshfydzd port=5432"
+	dsn := os.Getenv("DB_URL")
 	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
-
 	if err != nil {
 		logs.Logger.Fatal().Msgf("Failed to connect to database due to: %s", err.Error())
 	}
