cd524cb1fe5b16097f0af6637a033cd9a8ced6f8
Matei Cheles <mateicheles1@gmail.com>
2023-04-04 12:30:39 +0300

    code refactoring

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 func init() {
 	initializers.LoadEnvVariables()
 	// initializers.ConnectToDB()
+	// nu am cum sa termin db pentru ca nu am admin rights si imi tot arunca erori. maine o sa continui de pe laptopul personal
 }
  
 func main() {
