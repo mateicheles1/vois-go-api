cf65c4ed5588a8bbb69b5797a086ea70f04a7d0c
Matei Cheles <mateicheles1@gmail.com>
2023-04-25 17:16:28 +0300

    code refac

--- middlewares/error_handler.go
+++ middlewares/error_handler.go
@@ -9,18 +9,18 @@ import (
 func ErrorHandler() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		c.Next()
-		errorOccured := false
+
 		for _, err := range c.Errors {
 			logs.ErrorLogger.Error().
 				Str("Method", c.Request.Method).
 				Str("Path", c.Request.URL.Path).
 				Int("Status code", c.Writer.Status()).
 				Msgf("JSON syntax error in request body: %s", err.Error())
-			errorOccured = true
 		}
 
-		if errorOccured {
+		if len(c.Errors) > 0 {
 			c.JSON(c.Writer.Status(), "Error processing request: invalid JSON syntax in request body")
 		}
+
 	}
 }
